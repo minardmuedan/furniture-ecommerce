3be4f6ed7215c8cacaf6bdb252043ce8bc398892
minard <minardmuedan@gmail.com>
2025-10-01 20:05:02 +0800

    add auth signup table

---
+++ src/database/schema.ts
@@ -0,0 +1,44 @@
+import { boolean, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core'
+
+export const usersTable = pgTable('users', {
+  id: varchar().primaryKey(),
+  username: varchar().notNull(),
+  email: varchar().unique().notNull(),
+  emailVerified: timestamp('email_verified'),
+  password: varchar().notNull(),
+  avatarSrc: varchar('avatar_src'),
+  isAdmin: boolean('is_admin'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const sessionsTable = pgTable('sessions', {
+  id: varchar().primaryKey(),
+  userId: varchar()
+    .notNull()
+    .references(() => usersTable.id),
+  ipAddress: varchar('ip_address'),
+  userAgent: varchar('user_agent'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  expiresAt: timestamp('updated_at').notNull(),
+})
+
+export const emailVerificationsTable = pgTable('email-verifications', {
+  id: varchar().primaryKey(),
+  userId: varchar('user_id')
+    .notNull()
+    .references(() => usersTable.id),
+  token: varchar().unique().notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
+
+// export const passwordVerificationsTable = pgTable('password-verifications', {
+//   id: varchar().primaryKey(),
+//   userId: varchar('user_id')
+//     .notNull()
+//     .references(() => usersTable.id),
+//   token: varchar().unique().notNull(),
+//   expiresAt: timestamp('expires_at').notNull(),
+//   createdAt: timestamp('created_at').notNull().defaultNow(),
+// })
