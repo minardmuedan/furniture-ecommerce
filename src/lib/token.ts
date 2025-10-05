7e426c999fcf32b8cde77e973e7bb2e6397e2638
minard <minardmuedan@gmail.com>
2025-10-05 10:44:42 +0800

    change token configuration, AUTHHHH

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,7 +1,7 @@
 import { generateSecureRandomString, signJWT, verifyJWT } from './auth'
 
-const DB_TOKEN_EXPIRES_IN_MINUTE = 1
-const JWT_TOKEN_EXPIRES_IN_MINUTE = 1
+const DB_TOKEN_EXPIRES_IN_MINUTE = 15
+const JWT_TOKEN_EXPIRES_IN_MINUTE = 30
 
 export async function generateToken() {
   const expiresAt = new Date(Date.now() + 60_000 * DB_TOKEN_EXPIRES_IN_MINUTE)
